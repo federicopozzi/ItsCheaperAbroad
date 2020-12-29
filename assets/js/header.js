08a0799d20c7d95e6e033d52c92cb9ecfc1a2e1d
Federico <federico.izzop@gmail.com>
2020-12-29 12:07:13 +0100

    header and footer + things

---
+++ assets/js/header.js
@@ -0,0 +1,11 @@
+document.getElementById("header").innerHTML = `
+      <nav class="navbar d-flex justify-content-between">
+        <span><a class="navbar-brand WebsiteTitle" href="#">ITS-CHEAPER-ABROAD.COM</a></span>
+        <span><a class="navbar-brand WebsiteTitle" href="#">ITS-CHEAPER-ABROAD.COM</a></span>
+        <span><a class="navbar-brand WebsiteTitle" href="#">ITS-CHEAPER-ABROAD.COM</a></span>
+      </nav>
+      
+      <div class="firstTextContainer d-flex justify-content-center">
+        <span class="firstText">HAPPY NEW YEAR: 20% OFF IF YOU TAKE A PHOTOGRAPH OF YOURSELF AFTER SURGERY ON SELECTED ITEMS</span>
+      </div>
+      `
