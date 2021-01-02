488175ea430f3418b9e7b9a906339ada3b4f22b6
Federico <federico.izzop@gmail.com>
2021-01-02 15:08:19 +0100

    messapia + other things

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,11 +1,14 @@
 document.getElementById("header").innerHTML = `
-      <nav class="navbar d-flex justify-content-between">
-        <span><a class="navbar-brand WebsiteTitle" href="#">ITS-CHEAPER-ABROAD.COM</a></span>
-        <span><a class="navbar-brand WebsiteTitle" href="#">ITS-CHEAPER-ABROAD.COM</a></span>
-        <span><a class="navbar-brand WebsiteTitle" href="#">ITS-CHEAPER-ABROAD.COM</a></span>
-      </nav>
-      
-      <div class="firstTextContainer d-flex justify-content-center">
-        <span class="firstText">HAPPY NEW YEAR: 20% OFF IF YOU TAKE A PHOTOGRAPH OF YOURSELF AFTER SURGERY ON SELECTED ITEMS</span>
-      </div>
+<section class="navBackground">
+  <div class="container pt-3 pb-3">
+  <div class="row">
+    <div class="col-lg-12 d-flex justify-content-between navFont">
+<span>INTRO</span>
+<span>LOGO</span>
+<span>ABOUT</span>
+
+    </div>
+  </div>
+</div>
+</section>
       `
