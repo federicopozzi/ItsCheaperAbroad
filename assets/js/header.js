8f941276c0ee8af5a3de427402588c661d56a373
Federico <federico.izzop@gmail.com>
2021-01-04 16:55:59 +0100

    productpage + margin

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,6 +1,6 @@
 document.getElementById("header").innerHTML = `
 <section class="navBackground">
-  <div class="container pt-3 pb-3">
+  <div class="container-fluid pt-3 pb-3" style="padding-right:5rem; padding-left:5rem;">
   <div class="row">
     <div class="col-lg-12 d-flex justify-content-between navFont">
 <span>INTRO</span>
