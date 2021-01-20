b9b46d8b0a4abaa3e978d589b964bc2588b0b0be
Federico <federico.izzop@gmail.com>
2021-01-20 12:25:08 +0100

    beauty

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,8 +1,7 @@
-
 let prefi = document.location.hostname === "federicopozzi.github.io" ? "/phase3/" : "/phase3/";
 
-if (document.location.pathname === prefi + "home.html" || document.location.pathname === prefi + "index.html" ) {
-      document.getElementById("footer").innerHTML = `
+if (document.location.pathname === prefi + "home.html" || document.location.pathname === prefi + "index.html") {
+  document.getElementById("footer").innerHTML = `
 
 <section class="websiteMargin pt-3 pb-3">
 <div class="row">
@@ -13,18 +12,16 @@ if (document.location.pathname === prefi + "home.html" || document.location.path
      <img src="./assets/svg/polimi_white.svg" class="img-fluid" alt="Responsive image" width="200">
 </div>
 <div class="col-lg-5 col-7">
-     <span class="textFoot">© All pictures shown at Itscheaperabroad belong to Instagram. 
-Instagram is a registered trademark and it’s part of The Facebook Company. 
+     <span class="textFoot">© All pictures shown at Itscheaperabroad belong to Instagram.
+Instagram is a registered trademark and it’s part of The Facebook Company.
 Images are not modified nor altered. You may download anything on these pages for your own personal, non-commercial use.</span>
 </div>
 </div>
 </section>
-    
-      `
-}
 
-else {
-      document.getElementById("footer").innerHTML = `
+      `
+} else {
+  document.getElementById("footer").innerHTML = `
 
 <section class="websiteMargin pt-3 pb-3">
 <div class="row">
@@ -35,13 +32,13 @@ else {
      <img src="../assets/svg/polimi_white.svg" class="img-fluid" alt="Responsive image" width="200">
 </div>
 <div class="col-lg-5 col-7">
-     <span class="textFoot">© All pictures shown at Itscheaperabroad belong to Instagram. 
-Instagram is a registered trademark and it’s part of The Facebook Company. 
+     <span class="textFoot">© All pictures shown at Itscheaperabroad belong to Instagram.
+Instagram is a registered trademark and it’s part of The Facebook Company.
 Images are not modified nor altered. You may download anything on these pages for your own personal, non-commercial use.</span>
 </div>
 </div>
 </section>
-    
+
       `
 
 }
