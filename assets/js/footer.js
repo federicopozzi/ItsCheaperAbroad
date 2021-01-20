4edac3c8b6bded1509085b3b9c192b8206d278f5
Federico <federico.izzop@gmail.com>
2021-01-20 12:18:46 +0100

    footer

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -6,13 +6,13 @@ if (document.location.pathname === prefi + "home.html" || document.location.path
 
 <section class="websiteMargin pt-3 pb-3">
 <div class="row">
-     <div class="col-lg-3">
+     <div class="col-lg-3 col-2">
      <img src="./assets/svg/density_white.svg" class="img-fluid" alt="Responsive image" width="50">
 </div>
-<div class="col-lg-4">
+<div class="col-lg-4 col-3">
      <img src="./assets/svg/polimi_white.svg" class="img-fluid" alt="Responsive image" width="200">
 </div>
-<div class="col-lg-5">
+<div class="col-lg-5 col-7">
      <span class="textFoot">© All pictures shown at Itscheaperabroad belong to Instagram. 
 Instagram is a registered trademark and it’s part of The Facebook Company. 
 Images are not modified nor altered. You may download anything on these pages for your own personal, non-commercial use.</span>
@@ -28,13 +28,13 @@ else {
 
 <section class="websiteMargin pt-3 pb-3">
 <div class="row">
-     <div class="col-lg-3">
+     <div class="col-lg-3 col-2">
      <img src="../assets/svg/density_white.svg" class="img-fluid" alt="Responsive image" width="50">
 </div>
-<div class="col-lg-4">
+<div class="col-lg-4 col-3">
      <img src="../assets/svg/polimi_white.svg" class="img-fluid" alt="Responsive image" width="200">
 </div>
-<div class="col-lg-5">
+<div class="col-lg-5 col-7">
      <span class="textFoot">© All pictures shown at Itscheaperabroad belong to Instagram. 
 Instagram is a registered trademark and it’s part of The Facebook Company. 
 Images are not modified nor altered. You may download anything on these pages for your own personal, non-commercial use.</span>
