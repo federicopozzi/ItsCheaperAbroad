6e6f73f97effe4a8dea592dcd6d0c2586376cfb3
andrmart <77062421+andrmart@users.noreply.github.com>
2021-01-22 11:34:32 +0100

    tutti i Breast ma senza VIEW DEAL che aggiungo dopo
    bop bop bop bop to the top ma anche un po' meno

--- assets/js/prod.js
+++ assets/js/prod.js
@@ -56,7 +56,7 @@ switch (document.location.pathname) {
         cardOptions.firstBio = "📧: info@plastic-surgery-phuket.com";
         cardOptions.secondBio = "youtu.be/agpsVo6HF6A";
         cardOptions.secondBioLink = "http://youtu.be/agpsVo6HF6A";
-        cardOptions.country = "Thailand";
+        cardOptions.country = "THAILAND";
         cardOptions.viewProfile = 'https://www.instagram.com/plasticsurgeryphuket/\n';
         cardOptions.typeOp = 'TUMMY TUCK';
         cardOptions.price = "$ 6.900";
@@ -76,7 +76,7 @@ switch (document.location.pathname) {
         cardOptions.firstBio = "MD. Cirujano Plástico Estético";
         cardOptions.secondBio = "www.pachecoplastic.net";
         cardOptions.secondBioLink = "http://www.pachecoplastic.net";
-        cardOptions.country = "Colombia";
+        cardOptions.country = "COLOMBIA";
         cardOptions.viewProfile = 'https://www.instagram.com/plasticsurgeryphuket/\n';
         cardOptions.typeOp = 'LIPOSCULPTURE';
         cardOptions.price = "$ 3.000";
@@ -96,7 +96,7 @@ switch (document.location.pathname) {
         cardOptions.firstBio = "MD. Cirujano Plástico Estético";
         cardOptions.secondBio = "www.pachecoplastic.net";
         cardOptions.secondBioLink = "http://www.pachecoplastic.net";
-        cardOptions.country = "Colombia";
+        cardOptions.country = "COLOMBIA";
         cardOptions.viewProfile = 'https://www.instagram.com/plasticsurgeryphuket/\n';
         cardOptions.typeOp = 'ABDOMINOPLASTY';
         cardOptions.price = "$ 3.000";
@@ -116,7 +116,7 @@ switch (document.location.pathname) {
         cardOptions.firstBio = "Surgical Clinic & Wellness Center";
         cardOptions.secondBio = "";
         cardOptions.secondBioLink = "";
-        cardOptions.country = "Mexico";
+        cardOptions.country = "MEXICO";
         cardOptions.viewProfile = 'https://www.instagram.com/sirenesurgeryclinic/';
         cardOptions.typeOp = 'GASTRIC SLEEVE';
         cardOptions.price = "$ 5.500";
@@ -124,11 +124,111 @@ switch (document.location.pathname) {
         break;
 
     case pref + "breastOp01.html":
-        cardOptions.title = 'GASTRIC SLEEVE';
-        cardOptions.firstImg = '../assets/img/bellyAll/Belly_06_01.jpg';
-        cardOptions.secondImg = '../assets/img/bellyAll/Belly_06_02.jpg';
-        cardOptions.thirdImg = '../assets/img/bellyAll/Belly_06_03.jpg';
-        cardOptions.igImg = '../assets/img/bellyAll/IG_Belly_06.jpg';
+        cardOptions.title = 'BREAST AUGMENTATION';
+        cardOptions.firstImg = '../assets/img/breastAll/Breast_01_01.jpg';
+        cardOptions.secondImg = '../assets/img/breastAll/Breast_01_02.jpg';
+        cardOptions.thirdImg = '../assets/img/breastAll/Breast_01_03.jpg';
+        cardOptions.igImg = '../assets/img/breastAll/IG_Breast_01.jpg';
+        cardOptions.profile = '@beautiquekorea';
+        cardOptions.secondLine = 'BEAUTIQUEKOREA';
+        cardOptions.follower = '12.3k';
+        cardOptions.posts = "855";
+        cardOptions.firstBio = "The No.1 Medical Tourism Company in Korea!";
+        cardOptions.secondBio = "www.beautiquekorea.com";
+        cardOptions.secondBioLink = "https://www.beautiquekorea.com/";
+        cardOptions.country = "SOUTH KOREA";
+        cardOptions.viewProfile = 'https://www.instagram.com/beautiquekorea/';
+        cardOptions.typeOp = 'BREAST AUGMENTATION';
+        cardOptions.price = "$ 3.100";
+        cardOptions.viewDeal = '';
+        break;
+
+    case pref + "breastOp02.html":
+        cardOptions.title = 'BREAST AUGMENTATION';
+        cardOptions.firstImg = '../assets/img/breastAll/Breast_02_01.jpg';
+        cardOptions.secondImg = '../assets/img/breastAll/Breast_02_02.jpg';
+        cardOptions.thirdImg = '../assets/img/breastAll/Breast_02_03.jpg';
+        cardOptions.igImg = '../assets/img/breastAll/IG_Breast_02.jpg';
+        cardOptions.profile = '@plasticsurgeryphuket';
+        cardOptions.secondLine = 'Plastic Surgery Phuket';
+        cardOptions.follower = '1.2k';
+        cardOptions.posts = "497";
+        cardOptions.firstBio = "📧: info@plastic-surgery-phuket.com";
+        cardOptions.secondBio = "youtu.be/agpsVo6HF6A";
+        cardOptions.secondBioLink = "http://youtu.be/agpsVo6HF6A";
+        cardOptions.country = "THAILAND";
+        cardOptions.viewProfile = 'https://www.instagram.com/plasticsurgeryphuket/\n';
+        cardOptions.typeOp = 'BREAST AUGMENTATION';
+        cardOptions.price = "$ 3.844";
+        cardOptions.viewDeal = '';
+        break;
+
+    case pref + "breastOp03.html":
+        cardOptions.title = 'BREAST AUGMENTATION';
+        cardOptions.firstImg = '../assets/img/breastAll/Breast_03_01.jpg';
+        cardOptions.secondImg = '../assets/img/breastAll/Breast_03_02.jpg';
+        cardOptions.thirdImg = '../assets/img/breastAll/Breast_03_03.jpg';
+        cardOptions.igImg = '../assets/img/breastAll/IG_Breast_03.jpg';
+        cardOptions.profile = '@estetikinternational';
+        cardOptions.secondLine = 'Estetik International';
+        cardOptions.follower = '70.2k';
+        cardOptions.posts = "2.367";
+        cardOptions.firstBio = "WP TR 05323783845";
+        cardOptions.secondBio = "www.youtube.com/watch?v=o5nDI9i-uNw";
+        cardOptions.secondBioLink = "https://l.instagram.com/?u=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3Do5nDI9i-uNw&e=ATMBN_dNQi8PnVXWU1gkqPsWvWsk79uoOmwW3zZRXObRumoiMEnrh7bbF2DKQiI80VC_8RS7qMfiFerKuAGfeg&s=1";
+        cardOptions.country = "TURKEY";
+        cardOptions.viewProfile = 'https://www.instagram.com/estetikinternational/';
+        cardOptions.typeOp = 'BREAST AUGMENTATION';
+        cardOptions.price = "$ 4.473";
+        cardOptions.viewDeal = '';
+        break;
+
+    case pref + "breastOp04.html":
+        cardOptions.title = 'BREAST AUGMENTATION';
+        cardOptions.firstImg = '../assets/img/breastAll/Breast_04_01.jpg';
+        cardOptions.secondImg = '../assets/img/breastAll/Breast_04_02.jpg';
+        cardOptions.thirdImg = '../assets/img/breastAll/Breast_04_03.jpg';
+        cardOptions.igImg = '../assets/img/breastAll/IG_Breast_04.jpg';
+        cardOptions.profile = '@dradeniacarpio';
+        cardOptions.secondLine = 'Dra Denia Carpio';
+        cardOptions.follower = '4.934';
+        cardOptions.posts = "360";
+        cardOptions.firstBio = "Cirujana Plástica Reconstructiva y Estética";
+        cardOptions.secondBio = "";
+        cardOptions.secondBioLink = "";
+        cardOptions.country = "DOM. REP.";
+        cardOptions.viewProfile = 'https://www.instagram.com/dradeniacarpio/';
+        cardOptions.typeOp = 'BREAST AUGMENTATION';
+        cardOptions.price = "$ 2.500";
+        cardOptions.viewDeal = '';
+        break;
+
+    case pref + "breastOp05.html":
+        cardOptions.title = 'BREAST AUGMENTATION';
+        cardOptions.firstImg = '../assets/img/breastAll/Breast_05_01.jpg';
+        cardOptions.secondImg = '../assets/img/breastAll/Breast_05_02.jpg';
+        cardOptions.thirdImg = '../assets/img/breastAll/Breast_05_03.jpg';
+        cardOptions.igImg = '../assets/img/breastAll/IG_Breast_05.jpg';
+        cardOptions.profile = '@drasilviaaviles';
+        cardOptions.secondLine = 'DRA. SILVIA AVILÉS';
+        cardOptions.follower = '2.135';
+        cardOptions.posts = "278";
+        cardOptions.firstBio = "Whatsapp 8298618685";
+        cardOptions.secondBio = "linktr.ee/drasilviaaviles";
+        cardOptions.secondBioLink = "https://l.instagram.com/?u=https%3A%2F%2Flinktr.ee%2Fdrasilviaaviles&e=ATNPRuv5I_X1tEJT8-M80KYwX2VgsthsE_m-c4Vbq_sXEsFR-3UK6CWpSWaNekDuhgSC12gfNRF_D5iPoDrHkw&s=1";
+        cardOptions.country = "DOM. REP.";
+        cardOptions.viewProfile = 'https://www.instagram.com/drasilviaaviles/';
+        cardOptions.typeOp = 'BREAST AUGMENTATION';
+        cardOptions.price = "$ 4.320";
+        cardOptions.viewDeal = '';
+        break;
+
+    case pref + "breastOp06.html":
+        cardOptions.title = 'BREAST IMPLANTS';
+        cardOptions.firstImg = '../assets/img/breastAll/Breast_06_01.jpg';
+        cardOptions.secondImg = '../assets/img/breastAll/Breast_06_02.jpg';
+        cardOptions.thirdImg = '../assets/img/breastAll/Breast_06_03.jpg';
+        cardOptions.igImg = '../assets/img/breastAll/IG_Breast_06.jpg';
         cardOptions.profile = '@sirenesurgeryclinic';
         cardOptions.secondLine = 'Sirene Surgical Coordinator';
         cardOptions.follower = '81';
@@ -136,10 +236,10 @@ switch (document.location.pathname) {
         cardOptions.firstBio = "Surgical Clinic & Wellness Center";
         cardOptions.secondBio = "";
         cardOptions.secondBioLink = "";
-        cardOptions.country = "Mexico";
+        cardOptions.country = "MEXICO";
         cardOptions.viewProfile = 'https://www.instagram.com/sirenesurgeryclinic/';
-        cardOptions.typeOp = 'GASTRIC SLEEVE';
-        cardOptions.price = "$ 5.500";
+        cardOptions.typeOp = 'BREAST IMPLANTS';
+        cardOptions.price = "$ 3.500";
         cardOptions.viewDeal = '';
         break;
 
