38c2015d651fe10b9ab6ad4e5915baa03b8fadb3
Federico <federico.izzop@gmail.com>
2021-01-26 17:15:27 +0100

    banner home, header butt, carosel mobile, footer mobile

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -23,32 +23,32 @@ Images are not modified nor altered. You may download anything on these pages fo
 </section>
 </div>
 
-<div class="displayMobile">
-<section class="websiteMargin pt-5 pb-5 pr-2 mt-3">
-<div class="container-fluid">
-<div class="row pl-4 pt-4 pb-2 pr-3">
-     <div class="col-5">
-     <div class="row">
-     <div class="col-12 d-flex justify-content-center">
-     <img src="./assets/svg/density_white.svg" class="img-fluid" alt="Responsive image" width="50">
+  <div class="displayMobile">
+    <section class="websiteMargin pt-5 pb-5 pr-2 mt-3">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-5">
+            <div class="row">
+              <div class="col-12 d-flex justify-content-center">
+                <img src="./assets/svg/density_white.svg" class="img-fluid" alt="Responsive image" width="50">
 
-</div>
-<div class="col-12 pt-3 d-flex justify-content-left">
+              </div>
+              <div class="col-12 pt-3 d-flex justify-content-left">
 
-   <img src="./assets/svg/polimi_white.svg" class="img-fluid" alt="Responsive image" width="120">
-</div>
-</div>
+                <img src="./assets/svg/polimi_white.svg" class="img-fluid" alt="Responsive image" width="120">
+              </div>
+            </div>
 
-</div>
-<div class="col-lg-5 col-7">
+          </div>
+          <div class="col-lg-5 col-7">
      <span class="textFoot">© All pictures shown at Itscheaperabroad belong to Instagram.
 Instagram is a registered trademark and it’s part of The Facebook Company.
 Images are not modified nor altered. You may download anything on these pages for your own personal, non-commercial use.</span>
-</div>
-</div>
-</div>
-</section>
-</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 
 
       `
@@ -76,31 +76,31 @@ Images are not modified nor altered. You may download anything on these pages fo
 </div>
 
 <div class="displayMobile">
-<section class="websiteMargin pt-5 pb-5 pr-2 mt-3">
-<div class="container-fluid">
-<div class="row pl-4 pt-4 pb-2 pr-3">
-     <div class="col-5">
-     <div class="row">
-     <div class="col-12 d-flex justify-content-center">
-     <img src="../assets/svg/density_white.svg" class="img-fluid" alt="Responsive image" width="50">
+    <section class="websiteMargin pt-5 pb-5 pr-2 mt-3">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-5">
+            <div class="row">
+              <div class="col-12 d-flex justify-content-center">
+                <img src="../assets/svg/density_white.svg" class="img-fluid" alt="Responsive image" width="50">
 
-</div>
-<div class="col-12 pt-3 d-flex justify-content-left">
+              </div>
+              <div class="col-12 pt-3 d-flex justify-content-left">
 
-   <img src="../assets/svg/polimi_white.svg" class="img-fluid" alt="Responsive image" width="120">
-</div>
-</div>
+                <img src="../assets/svg/polimi_white.svg" class="img-fluid" alt="Responsive image" width="120">
+              </div>
+            </div>
 
-</div>
-<div class="col-lg-5 col-7">
+          </div>
+          <div class="col-lg-5 col-7">
      <span class="textFoot">© All pictures shown at Itscheaperabroad belong to Instagram.
 Instagram is a registered trademark and it’s part of The Facebook Company.
 Images are not modified nor altered. You may download anything on these pages for your own personal, non-commercial use.</span>
-</div>
-</div>
-</div>
-</section>
-</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 
 
       `
